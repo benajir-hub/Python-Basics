119e43be1bb7f218ea41915b7bc2fef6cfc5cceb
benajir-hub <banajir.shuvo@gmail.com>
2022-03-31 15:25:12 +0200

    Python files added

---
+++ Python/additionOfTwoNumbers.py
@@ -0,0 +1,17 @@
+first_num = 10 
+second_num = 20
+first_guy = "Rahim"
+second_guy = "Karim"
+
+addition = first_num + second_num
+print(addition)
+
+print("Type of first number: ", type(first_num), "Type of second number: ", type(second_num), type(first_guy), type(second_guy))
+
+a=10
+b=10
+
+print("Memory Location of first number: ", id(a), "MemoryLocation of second number: ", id(a), )
+
+
+#Memory Intering
---
+++ Python/first_code.py
@@ -0,0 +1,5 @@
+print ("Welcome to python!!")
+
+
+# ctrl +s = To save the program
+# ctrl +b = To execute the program
