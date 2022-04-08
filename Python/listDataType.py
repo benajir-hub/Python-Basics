93c716a27deea6cc27f812b9aabbb3f30ab5bc93
benajir-hub <banajir.shuvo@gmail.com>
2022-04-08 19:28:46 +0200

    List data type [Sequential]

---
+++ Python/listDataType.py
@@ -0,0 +1,8 @@
+list1 = [1,'h', 'Python', 2]
+print(list1) #printing the given list
+print(id(list1)) #memory location of the list
+print(type(list1)) #type of given list
+print(list1[2:]) #list slicing
+print(list1[0:2]) #list slicing
+print(list1 + list1) #list concatenation using + operator
+print(list1*3) #List repeation using * operator
