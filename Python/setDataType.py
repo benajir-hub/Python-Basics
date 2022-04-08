52c4b0fed540948bc9111e297876652d6b958941
benajir-hub <banajir.shuvo@gmail.com>
2022-04-08 20:09:47 +0200

    Set data Type

---
+++ Python/setDataType.py
@@ -0,0 +1,13 @@
+set1 = set()#empty set creation
+
+print(set1)
+
+set2 = {'James', 2, 45, 'Python', 'k'}
+print(set2)
+print(type(set2))
+
+set2.add(100)#Adding element to the set
+print(set2)
+
+set2.remove(2)#Removing element from the set
+print(set2)
