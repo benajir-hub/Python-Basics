12531a84e115ec67e5ebebdea4fa45f852c9cdda
benajir-hub <banajir.shuvo@gmail.com>
2022-04-08 18:20:02 +0200

    Numbers Data Type

---
+++ Python/numbersDataType.py
@@ -0,0 +1,10 @@
+a = 10
+print("Type of a",type(a))
+
+b = 4.5
+print("Type of b", type(b))
+print("Instance of b, Floating number?", isinstance(b,float))
+
+c = 1+3j
+print("Type of c", type(c))
+print("C is a Complex number:", isinstance(c,complex))
